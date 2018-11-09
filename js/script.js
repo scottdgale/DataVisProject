47fc03a018bc0033218e0aec0d452a2e90bf7711
Scott Gale <scottdgale@gmail.com>
2018-11-08 18:40:48 -0700

    Saved progess

--- js/script.js
+++ js/script.js
@@ -1,102 +1,101 @@
-loadMapData().then(data =>{
+loadMapData().then(data => {
 
-let mapData = data[0];
-let cityData = data[1];
+    let mapData = data[0];
+    let cityData = data[1];
 
-console.log(mapData);
+    console.log(mapData);
 
-//Create instances of objects here
-let map = new Map(syncData);
+    //Create instances of objects here
+    let map = new Map(syncData);
 
-let balanceSingle = new Balance_Single();
+    let balanceSingle = new Balance_Single();
 
-let topTraders = new Top_Traders();
+    let topTraders = new Top_Traders();
 
-let globalBalance = new Global_Balance();
+    let globalBalance = new Global_Balance();
 
-let balanceDouble = new Balance_Double();
+    let balanceDouble = new Balance_Double();
 
-let selected_years = ["2000", "2005"];
+    let selected_years = ["2000", "2005"];
 
-//Change to use country "id" . . .
-let primary = "USA";
+    //Change to use country "id" . . .
+    let primary = "USA";
 
-let secondary = "CHN";
+    let secondary = "CHN";
 
-let gdpDataSet = d3.csv("data/gdp.csv");
+    let gdpDataSet = d3.csv("data/gdp.csv");
 
-//UPDATE WITH DIFFERENT COLORS - Domain definition for global color scale
-let domain = [-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60];
-//Color range for global color scale
-let range = ["#063e78", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15", "#860308"];
-//ColorScale be used consistently by all the charts
-let colorScale = d3.scaleQuantile()
-    .domain(domain)
-    .range(range);
+    //UPDATE WITH DIFFERENT COLORS - Domain definition for global color scale
+    let domain = [-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60];
+    //Color range for global color scale
+    let range = ["#063e78", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15", "#860308"];
+    //ColorScale be used consistently by all the charts
+    let colorScale = d3.scaleQuantile()
+        .domain(domain)
+        .range(range);
 
-//SyncData with initial dataset - All objects will call syncData for interaction
-syncData(primary, secondary, selected_years);
-
-
-//syncData is the focal point for all interactions and updates
-function syncData(priCountry, secCountry, years){
-    primary = priCountry;
-    secondary = secCountry;
-    selected_years = years;
+    //SyncData with initial dataset - All objects will call syncData for interaction
+    syncData(primary, secondary, selected_years);
 
 
-    let dataDyadic = loadDataDyadic();
-    loadDataNational();  //load gdp occurs in loadDataNational
+    //syncData is the focal point for all interactions and updates
+    function syncData(priCountry, secCountry, years) {
+        primary = priCountry;
+        secondary = secCountry;
+        selected_years = years;
 
-    //Call update for all views - ONLY pass the data you need to increase system performance
-    map.update(dataDyadic, primary, secondary, selected_years, mapData, cityData);
-}
 
+        let dataDyadic = loadDataDyadic();
+        loadDataNational();  //load gdp occurs in loadDataNational
 
-//National
-function loadDataNational () {
-    let nationalArray = [];
-    let count = selected_years[1]-selected_years[0];
-    for (let year = selected_years[0]; year <= (selected_years[1]); year++) {
-        d3.csv("data/National/National_" + year + ".csv").then(nationalData => {
-            nationalArray.push(nationalData);
-            if(!count--) {
-                d3.csv("data/gdp.csv").then(gdpData => {
-                    globalBalance.update(nationalArray, gdpData, primary, secondary, selected_years);
-                });
-            }
-        });
+        //Call update for all views - ONLY pass the data you need to increase system performance
+        map.update(dataDyadic, primary, secondary, selected_years, mapData, cityData);
     }
-}
 
-//Dyadic
-function loadDataDyadic (year) {
-    let dyadicArray = [];
-    let count = selected_years[1] - selected_years[0];
-    for (let year = selected_years[0]; year <= selected_years[1]; year++) {
-        d3.csv("data/Dyadic/Dyadic_" + year + ".csv").then(d => {
-            dyadicArray.push(d);
-            //Once all years are loaded - call the appropriate .update functions.
-            if (!count--) {
-                topTraders.update(dyadicArray, primary, secondary, selected_years);
-                balanceSingle.update(dyadicArray, primary, secondary, selected_years);
-                balanceDouble.update(dyadicArray, gdpDataSet, primary, secondary, selected_years);
-            }
-        });
+
+    //National
+    function loadDataNational() {
+        let nationalArray = [];
+        let count = selected_years[1] - selected_years[0];
+        for (let year = selected_years[0]; year <= (selected_years[1]); year++) {
+            d3.csv("data/National/National_" + year + ".csv").then(nationalData => {
+                nationalArray.push(nationalData);
+                if (!count--) {
+                    d3.csv("data/gdp.csv").then(gdpData => {
+                        globalBalance.update(nationalArray, gdpData, primary, secondary, selected_years);
+                    });
+                }
+            });
+        }
     }
-    return dyadicArray;
-}
+
+    //Dyadic
+    function loadDataDyadic(year) {
+        let dyadicArray = [];
+        let count = selected_years[1] - selected_years[0];
+        for (let year = selected_years[0]; year <= selected_years[1]; year++) {
+            d3.csv("data/Dyadic/Dyadic_" + year + ".csv").then(d => {
+                dyadicArray.push(d);
+                //Once all years are loaded - call the appropriate .update functions.
+                if (!count--) {
+                    topTraders.update(dyadicArray, primary, secondary, selected_years);
+                    balanceSingle.update(dyadicArray, primary, secondary, selected_years);
+                    balanceDouble.update(dyadicArray, gdpDataSet, primary, secondary, selected_years);
+                }
+            });
+        }
+        return dyadicArray;
+    }
 
 
-let cities = [];
-d3.csv('Data/capital_cities.csv').then(capitalCityData =>{
-    cities.push(capitalCityData);
+    let cities = [];
+    d3.csv('Data/capital_cities.csv').then(capitalCityData => {
+        cities.push(capitalCityData);
+    });
+
 });
 
-
-});
-
-async function loadMapData(){
+async function loadMapData() {
     let mapData = await d3.json('Data/world.json');
     let cityData = await d3.csv('Data/capital_cities.csv');
     let arr = [];
@@ -104,4 +103,7 @@ async function loadMapData(){
     arr.push(cityData);
     return arr;
 }
+
+
+
 
--- js/top_traders.js
+++ js/top_traders.js
@@ -5,6 +5,10 @@ class Top_Traders {
      * @param
      */
     constructor() {
+
+        this.totalExports = 0.0;
+        this.totalImports = 0.0;
+        this.flow1 = true; //true indicates the primary country is associated with (flow1=import)
         this.margin = {top: 20, right: 20, bottom: 20, left: 50, spacing: 57};
 
         let divTopTraders = d3.select("#top_traders").classed("half_view", true);
@@ -31,30 +35,64 @@ class Top_Traders {
             dataExport.push(this.sortSingleYearExport(data,pri,index).splice(0,50));
         }
         console.log(dataImport);
-        console.log(dataExport);
         //Average the data over the years
-
-
+        let topImportData = this.average(dataImport);
+        let topExportData = this.average(dataExport);
+        //console.log(this.totalImports);
+        //console.log(this.totalExports);
 
 
     }
 
+    average (sortedArray){
+        console.log(sortedArray);
+        let newAverage = [];
+        for (let c=0; c<sortedArray[0].length; c++){
+            //use the elements in the most recent year as the pivot id
+            let pivot = this.flow1? sortedArray[sortedArray.length-1][c].id2 : sortedArray[0][c].id1;
+            //add all the values from all year arrays for a particular country
+            sortedArray.forEach((year)=>{
+                if (this.flow1){
+                    year.forEach((country)=>{
+                        if (pivot===country.id2);
+                            //console.log (pivot + " " + country.id2 + " " + country.flow2);
+                            //sum the values for the same country and store in the newAverage array
+
+                    })
+
+                }
+
+            })
+        }
+
+    }
+
+
     sortSingleYearImport(data, pri, index){
         let singleYear = [];
-        let flow;
+        let localFlow;
+        //index represents the year
         for (let a=0; a<data[index].length; a++){
             if (data[index][a].id1 === pri){
                 singleYear.push(data[index][a]);
-                flow = 1;
+                localFlow = this.flow1 = true;
+                //Eliminate any missing data and sum the total imports
+                if (!(data[index][a].flow1==="-9")){
+                    this.totalImports += parseFloat(data[index][a].flow1);
+                }
             }
             else if (data[index][a].id2 === pri){
                 singleYear.push(data[index][a]);
-                flow = 2;
+                localFlow = this.flow1 = false;
+                //Eliminate any missing data and sum the total imports
+                if (!data[index][a].flow2==="-9") {
+                    this.totalImports += parseFloat(data[index][a].flow2);
+                }
             }
         }
         //sort data based on imports / exports
         singleYear.sort(function(a,b){
-            if (flow===1){
+            if (localFlow){
                 return b.flow1-a.flow1;
             }
             else
@@ -65,20 +103,28 @@ class Top_Traders {
 
     sortSingleYearExport(data, pri, index){
         let singleYear = [];
-        let flow;
+        let localFlow;
         for (let a=0; a<data[index].length; a++){
             if (data[index][a].id1 === pri){
                 singleYear.push(data[index][a]);
-                flow = 1;
+                localFlow = this.flow1 = true;
+                //Eliminate any missing data and sum the total exports
+                if (!(data[index][a].flow2==="-9")){
+                    this.totalExports += parseFloat(data[index][a].flow2);
+                }
             }
             else if (data[index][a].id2 === pri){
                 singleYear.push(data[index][a]);
-                flow = 2;
+                localFlow = this.flow1 = false;
+                //Eliminate any missing data and sum the total exports
+                if (!(data[index][a].flow1==="-9")){
+                    this.totalExports += parseFloat(data[index][a].flow1);
+                }
             }
         }
         //sort data based on imports / exports
         singleYear.sort(function(a,b){
-            if (flow===1){
+            if (localFlow){
                 return b.flow2-a.flow2;
             }
             else
@@ -86,5 +132,4 @@ class Top_Traders {
         });
         return singleYear;
     }
-
 }
