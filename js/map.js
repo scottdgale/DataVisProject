582f06086fae44bf5801472d32fbc56bed322070
sudiemroweton <42615519+sudiemroweton@users.noreply.github.com>
2018-11-20 21:39:47 -0700

    year bar, come on!

--- js/map.js
+++ js/map.js
@@ -81,8 +81,8 @@ class Map {
     }
 
     drawYearBar(){
-        let offset = (this.svgWidth - (this.yearData.length * 35))/2 //Create year bar in the center of the map svg
-        let distanceBetweenYears = 35;
+        let offset = (this.svgWidth - (this.yearData.length * 20))/2 //Create year bar in the center of the map svg
+        let distanceBetweenYears = 20;
         let tickWidth = 2;
         let initialBrushPlacement = [10*distanceBetweenYears + offset - tickWidth , 15*distanceBetweenYears + offset + tickWidth]
    
